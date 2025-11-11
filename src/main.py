e5addfca8375f375087f00ff494775c57c073fa5
Jos Dornschneider-Elkink <jelkink@gmail.com>
2025-11-11 08:57:39 +0000

    Allow for multiple input files

--- src/main.py
+++ src/main.py
@@ -6,18 +6,19 @@ from test import Test
 def main():
 
   parser = argparse.ArgumentParser()
-  parser.add_argument("-i", "--input", dest="filename", required=True)
   parser.add_argument("-d", "--delimiter", dest="delimiter", default=",")
   parser.add_argument("-e", "--no-header", action="store_true",  dest="header")
   parser.add_argument("-s", "--start", dest="start", default = "1")
   parser.add_argument("-n", "--number", dest="number", default = "-1")
   parser.add_argument("-a", "--audio", action="store_true", dest="audio", default=True)
   parser.add_argument("-t", "--type", action="store_true", dest="type", default=True)
+  parser.add_argument("filenames", nargs='+', help="Input file names")
   args = parser.parse_args()
 
   word_list = WordList()
   try:
-    word_list.read_file(args.filename, args.delimiter, args.header, int(args.start), int(args.number))
+    for filename in args.filenames:
+      word_list.read_file(filename, args.delimiter, args.header, int(args.start), int(args.number))
     word_list.shuffle_words()
 
     test = Test(word_list)
