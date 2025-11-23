1173f9b7e2a1498c3c6a2db7a13d0ac9a1fb0a3b
Jos Dornschneider-Elkink <jelkink@gmail.com>
2025-11-23 07:18:45 +0000

    Protect against using unavailable speech language

--- src/speech.py
+++ src/speech.py
@@ -14,7 +14,13 @@ class Speech:
     def __init__(self):
         pass
 
+    def has_language(self, language):
+        return language in languages
+
     def say(self, text, language):
+        if (not self.has_language(language)):
+            raise ValueError(f"Language '{language}' not supported for speech synthesis.")
+        
         print("🔊", end="", flush=True)
         text_hash = hashlib.md5(text.encode('utf-8')).hexdigest()
         soundfile = f"/tmp/vocab_{text_hash}_{language}.mp3"
